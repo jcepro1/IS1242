55566dad193367dab6dd0391244deb86cece7410
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2024-05-15 08:11:59 -0400

    Add files via upload

---
+++ Examensbsi - repe/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examensb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Vector v1, v2, v3,v4;
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            v1 = new Vector();
+            v2 = new Vector();
+            v3 = new Vector();
+            v4 = new Vector();
+         
+        }
+
+        private void cargarRndToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            v1.Cargar(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            textBox4.Text = v1.Descargar();
+        }
+
+        private void cargarManualToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            v1.cargardato(int.Parse(textBox1.Text));
+            textBox4.Text = v1.Descargar();
+        }
+
+        private void descargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = v1.Descargar();
+        }
+
+        private void cargarRndToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            v2.Cargar(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            textBox5.Text = v2.Descargar();
+        }
+
+        private void cargarManualToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            v2.cargardato(int.Parse(textBox1.Text));
+            textBox5.Text = v2.Descargar();
+        }
+
+        private void descargarToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = v2.Descargar();
+        }
+
+        private void cargarRndToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            v3.Cargar(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            textBox6.Text = v3.Descargar();
+        }
+
+        private void cargarManualToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            v3.cargardato(int.Parse(textBox1.Text));
+            textBox6.Text = v3.Descargar();
+        }
+
+        private void descargarToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = v3.Descargar();
+        }
+
+        private void descargarToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            //V4
+            textBox7.Text = v4.Descargar();
+        }
+
+        private void eJERCICIO1ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+        
+           
+
+
+        }
+
+
+
+
+
+
+        private void eJERCICIO2ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //2examen
+            v1.OrdenarEspiralInterno(int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            textBox5.Text = v1.Descargar();
+
+        }
+    }
+}
---
+++ Examensbsi - repe/Vector.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examensb
+{
+    class Vector
+    {
+        const int MAX = 50;
+        private int[] v;
+        private int n;
+
+        public Vector()
+        {
+            n = 0;
+            v = new int[MAX];
+        }
+
+        public void Cargar(int n1, int a, int b)
+        {
+            Random r = new Random();
+            int i;
+            n = n1;
+            for (i = 1; i <= n; i++)
+            {
+                v[i] = r.Next(a, b);
+            }
+        }
+
+        public void Cargar(int ele)
+        {
+            n++;
+            v[n] = ele;
+        }
+
+        public void cargardato(int nele)
+        {
+            n = nele;
+            int num = n;
+            for (int i = 1; i <= num; i++)
+            {
+                v[i] = Conversions.ToInteger(Interaction.InputBox(" ", " "));
+            }
+        }
+
+        public string Descargar()
+        {
+            string s = "";
+            int i;
+            for (i = 1; i <= n; i++)
+            {
+                s = s + v[i] + " | ";
+            }
+            return s;
+        }
+
+        //EXAMEN
+
+        //PREGUNTA 1
+
+
+
+
+        //PREGUNTA 2
+
+
+        public void OrdenarEspiralInterno(int a, int b)
+        {
+            // Ordenar el rango a, b
+            OrdBurbuja(a, b);
+
+            // Crear un array temporal para almacenar los elementos ordenados
+            int[] temp = new int[b - a + 1];
+            for (int i = a; i <= b; i++)
+            {
+                temp[i - a] = v[i];
+            }
+
+            // Punteros para las posiciones izquierda y derecha
+            int izquierda = a;
+            int derecha = b;
+
+            // Alternar la colocación de elementos de dos en dos desde el más pequeño
+            bool turnoIzquierda = true;
+            for (int i = 0; i < temp.Length; i += 2)
+            {
+                if (turnoIzquierda)
+                {
+                    if (i < temp.Length)
+                    {
+                        v[izquierda] = temp[i];
+                        izquierda++;
+                    }
+                    if (i + 1 < temp.Length)
+                    {
+                        v[izquierda] = temp[i + 1];
+                        izquierda++;
+                    }
+                }
+                else
+                {
+                    if (i < temp.Length)
+                    {
+                        v[derecha] = temp[i];
+                        derecha--;
+                    }
+                    if (i + 1 < temp.Length)
+                    {
+                        v[derecha] = temp[i + 1];
+                        derecha--;
+                    }
+                }
+                turnoIzquierda = !turnoIzquierda;
+            }
+        }
+
+
+        public void OrdBurbuja(int a, int b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j < b - (i - a); j++)
+                {
+                    if (v[j] > v[j + 1])
+                    {
+                        Intercambio(j, j + 1);
+                    }
+                }
+            }
+        }
+
+        public void Intercambio(int a, int b)
+        {
+            int aux = v[a];
+            v[a] = v[b];
+            v[b] = aux;
+        }
+
+    }
+
+}
+
